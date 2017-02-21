fc3ef0029d4342496b3b11c153140f1baa9f7564
Jeffrey Honig <jch@honig.net>
2017-02-20 20:22:00 -0500

    Refine distances
    And fix indentation

--- src/org/usfirst/frc/team5253/robot/autocommands/GearCenterAuto.java
+++ src/org/usfirst/frc/team5253/robot/autocommands/GearCenterAuto.java
@@ -30,9 +30,9 @@ public class GearCenterAuto extends CommandGroup {
         // arm.
 
 
-			addSequential(new DriveToDistance(-0.25, 64));
-			addSequential(new DropGearAuto());
-			addSequential(new DriveToDistance(.25, 24));
+    	addSequential(new DriveToDistance(1, 69));
+    	addSequential(new DropGearAuto());
+    	addSequential(new DriveToDistance(1, -24));
 			
     }
 }
