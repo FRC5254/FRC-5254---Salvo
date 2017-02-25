10e53f0de422cf818b80ef13d2e5211c1a23bbfb
Rorlipp <Rory.home89@gmail.com>
2017-02-25 16:04:54 -0500

    Diffs for shooting

--- src/org/usfirst/frc/team5253/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team5253/robot/subsystems/Shooter.java
@@ -22,7 +22,15 @@ public class Shooter extends Subsystem {
 	CANTalon shooterMotorTopRight = new CANTalon(RobotMap.SHOOTER_MOTOR_TOP_RIGHT);
 	CANTalon shooterMotorBottom = new CANTalon(RobotMap.SHOOTER_MOTOR_BOTTOM);
 	
+	Boolean shootingSpeedAchieved = false;
+	double minShooting  = 1000000.0;
+	double maxShooting  = 0;
+	
+	Boolean idleAchieved = false;
 	int loop, maxError, minError = 10000000;
+	double minIdle = 10000000.0;
+	double maxIdle = 0.0;
+	
 	
 	public Shooter() {
 		
@@ -42,7 +50,7 @@ public class Shooter extends Subsystem {
 		shooterMotorTopRight.reverseOutput(false);
 		
 		shooterMotorBottom.changeControlMode(CANTalon.TalonControlMode.PercentVbus);;
-		shooterMotorBottom.configNominalOutputVoltage(+0.0f, -0.0f);
+		shooterMotorBottom .configNominalOutputVoltage(+0.0f, -0.0f);
 		shooterMotorBottom.configPeakOutputVoltage(0.0f,-12.0f);
 		shooterMotorBottom.reverseSensor(false);
 	}
@@ -69,10 +77,22 @@ public class Shooter extends Subsystem {
 
     	SmartDashboard.putNumber("Spin Up RPM", shooterMotorTopLeft.getSpeed());
     	SmartDashboard.putNumber("Spin Up Error", shooterMotorTopLeft.getClosedLoopError());
+    	
+    	if (shooterMotorTopLeft.getSpeed() >= shooterRpm) {
+    		idleAchieved = true;
+    	}
+    	
+    	if (idleAchieved = true) {
+    		maxIdle = Math.max(maxIdle, shooterMotorTopLeft.getSpeed());
+    		minIdle = Math.min(minIdle, shooterMotorTopLeft.getSpeed());
+    		SmartDashboard.putString("DB/String 2", Double.toString(minIdle));
+    		SmartDashboard.putString("DB/String 7", Double.toString(maxIdle));
+    	}
+    	
     }
     
     
-    public void startShooting(double shooterRpm) {
+    public void startShooting(double shooterRpm) { 
     	
     	shooterMotorTopLeft.changeControlMode(CANTalon.TalonControlMode.Speed);
 		shooterMotorTopLeft.configPeakOutputVoltage(0.0f,-12.0f);
@@ -98,12 +118,32 @@ public class Shooter extends Subsystem {
     	
     	SmartDashboard.putNumber("Start Shooting RPM", shooterMotorTopLeft.getSpeed());
     	SmartDashboard.putNumber("Start Shooting Error", shooterMotorTopLeft.getClosedLoopError());
+    	
+    	if (shooterMotorTopLeft.getSpeed() >= shooterRpm){
+    		shootingSpeedAchieved = true;
+    	}
+    	
+    	if (shootingSpeedAchieved = true){
+    		maxShooting = Math.max(maxShooting, shooterMotorTopLeft.getSpeed());
+    		minShooting = Math.min(minShooting, shooterMotorTopLeft.getSpeed());
+    	
+    		SmartDashboard.putString("DB/String 3", Double.toString(minShooting));
+    		SmartDashboard.putString("DB/String 8", Double.toString(maxShooting));
+    	}
     }
     
     public void stopShooting(int RPM) {
     	
     	shooterMotorTopLeft.configPeakOutputVoltage(0.0f,0.0f);
     	shooterMotorTopLeft.set(0.0);
+    	
+    	minIdle = 10000000.0;
+    	maxIdle = 0.0;
+    	idleAchieved = false;
+    	
+    	shootingSpeedAchieved = false;
+    	minShooting  = 1000000.0;
+    	maxShooting  = 0;
     	
     	if (minError != 10000000 || maxError != 0) {
     		System.out.format("Stop: minError %d maxError %d%n", minError, maxError);
